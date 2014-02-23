a70427cb0559eb635f0e53f3950245c7c5d69a52
zuosanshao <smeite@qq.com>
2014-02-23 23:56:40 +0800

    add diagram page template

---
+++ app/controllers/Diagrams.scala
@@ -0,0 +1,20 @@
+package controllers
+
+import play.api.mvc.{Action, Controller}
+import controllers.users.Users
+
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: zuosanshao
+ * Date: 14-2-23
+ * Time: 下午11:37
+ */
+object  Diagrams extends Controller {
+
+  /* 图说 */
+  def diagram(id:Long) = Users.UserAction{ user => implicit request =>
+    Ok(views.html.diagrams.diagram(user))
+  }
+
+}
---
+++ public/assets/js/hiwowo/diagram/diagram.js
@@ -0,0 +1,7 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: zuosanshao
+ * Date: 14-2-23
+ * Time: 下午11:42
+ *
+ */
