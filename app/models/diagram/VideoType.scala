cba5aa0199476338925e9480456a4fcf8468b7af
zuosanshao <smeite@qq.com>
2014-07-31 23:49:03 +0800

    adjust

--- app/models/diagram/VideoType.scala
+++ app/models/diagram/VideoType.scala
@@ -13,6 +13,5 @@ object VideoType extends Enumeration{
   val FUNNY = Value("开心宠物")
   val KID = Value("萌童逗宠")
   val ADULT = Value("二货主人")
-  val EMOTION = Value("表情帝")
   val OTHER = Value("其他")
 }
